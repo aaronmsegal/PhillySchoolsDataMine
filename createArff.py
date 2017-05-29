d34c262275830bfee2a923619fd62779ba6de785
Micah Chao <chaom1994@gmail.com>
2017-05-29 16:59:33 -0400

    Cleanup

--- createArff.py
+++ createArff.py
@@ -1,16 +1,15 @@
 import json
 
-def writeToArff():
+def writeToArff(filename):
   parsedCombinedJson = {}
   # get dictionaries of all schools from output file
-  with open('combined_zip_output.json', 'r') as jsonFile:
+  with open(filename, 'r') as jsonFile:
     jsonString = jsonFile.read()
     parsedCombinedJson = json.loads(jsonString)
 
   values = parsedCombinedJson.values() # gets a list of attr:value dictionaries
   attrList = values[0].keys() # gets just the attribute names, note it might not be sorted
 
-  #print type(attrList)
   with open('schools.arff', 'w') as arffFile:
     # write comments if we want to
 
@@ -25,19 +24,16 @@ def writeToArff():
     # write data
     arffFile.write('@data\n')
     for school in parsedCombinedJson:
-      print school
       for i in range(len(attrList)):
         attr = attrList[i]
-        print attr
         attrValue = parsedCombinedJson[school][attr]
-        print attrValue
         line = attrValue + ', '
         if i == len(attrList)-1:
           line = attrValue + '\n\n'
         arffFile.write(line)
 
 def main():
-  writeToArff()
+  writeToArff('combined_zip_output.json')
 
 if __name__ == "__main__":
   main()
