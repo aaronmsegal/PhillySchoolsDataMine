c039d3248c25fc90bd889ff5e8e35f279a968793
Jonathan McDaniel <jonathantmcdaniel@gmail.com>
2017-05-29 10:49:49 -0400

    Adding greatschools rating python script

---
+++ greatschools_ratings.py
@@ -0,0 +1,57 @@
+import xml.etree.ElementTree
+import json
+import sys
+
+def parseXML():
+    tree = xml.etree.ElementTree.parse('browseschoolsdata.xml')
+    schoolsDict = {}
+    schools = tree.findall('school')
+    for school in schools:
+        schoolAttrDict = {}
+        # Load Attributes
+        name = school.find('name').text
+        schoolType = school.find('type').text
+        gsRatingRaw = school.find('gsRating')
+        if gsRatingRaw is not None:
+            gsRating = gsRatingRaw.text
+        else:
+            gsRating = "No GreatSchools Rating"
+        parentRatingRaw = school.find('parentRating')
+        if parentRatingRaw is not None:
+            parentRating = parentRatingRaw.text
+        else:
+            parentRating = "No Parent Rating"
+        # Build dictionary
+        schoolAttrDict['name'] = name
+        schoolAttrDict['type'] = schoolType
+        schoolAttrDict['gsRating'] = gsRating
+        schoolAttrDict['parentRating'] = parentRating
+        schoolsDict[name] = schoolAttrDict
+        # Print Attributes
+        #print name
+        #print "GS Rating: " + gsRating
+        #print "Parent Rating " + parentRating
+        #print ""
+    # Print JSON
+    return json.dumps(schoolsDict)
+
+def combineJsonFiles(filename, internalJson):
+    with open(filename, 'r') as externalFile:
+        externalJsonString = externalFile.read()
+        externalJson = json.loads(externalJsonString)
+
+    for schoolName in internalJson.keys():
+        for attribute in externalJson[schoolName].keys():
+            if not attribute in internalJson[schoolName].keys(): # only add attributes not found in previous file
+                internalJson[schoolName][attribute] = externalJson[schoolName][attribute]
+
+    with open('combined.json', 'w') as outputFile:
+        json.dump(internalJson, outputFile, sort_keys = True)
+
+def main():
+    externalFile = sys.argv[1]
+    ratingsJson = parseXML()
+    combineJsonFiles(externalFile, ratingsJson)
+
+if __name__ == "__main__":
+    main()
