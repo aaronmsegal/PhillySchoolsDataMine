e3338786801e75a9e91a6a7809e2f0191b7ba6a0
Micah Chao <chaom1994@gmail.com>
2017-06-01 17:41:12 -0400

    Added function to wrap things around quotes

--- createArff.py
+++ createArff.py
@@ -10,7 +10,7 @@ def writeToArff(filename):
   values = parsedCombinedJson.values() # gets a list of attr:value dictionaries
   attrList = values[0].keys() # gets just the attribute names, note it might not be sorted
 
-  with open('schools.arff', 'w') as arffFile:
+  with open('schools_master.arff', 'w') as arffFile:
     # write comments if we want to
 
     # write relation
@@ -27,13 +27,19 @@ def writeToArff(filename):
       for i in range(len(attrList)):
         attr = attrList[i]
         attrValue = parsedCombinedJson[school][attr]
-        line = '{0}, '.format(attrValue)
+        line = '{0}, '.format(wrapInQuotes(attrValue))
         if i == len(attrList)-1:
-          line = '{0}\n\n'.format(attrValue)
+          line = '{0}\n\n'.format(wrapInQuotes(attrValue))
         arffFile.write(line)
 
+def wrapInQuotes(s):
+  qString = s
+  if type(s) is unicode and "?" not in s:
+    qString = '\"' + s + '\"'
+  return qString
+
 def main():
-  writeToArff('SchoolProgressAdded.json')
+  writeToArff('masterfile.json')
 
 if __name__ == "__main__":
   main()
